3def59571c9ec023f1ffe661c988631baa291bf3
Lynda <lylyben74@gmail.com>
2024-07-09 11:28:49 +0200

    add courses

---
+++ resources/js/Components/Track.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'Track',
+    emits : ['played'],
+    props: {
+        track: Object
+    },
+    methods : {
+        handleClick(){
+            this.$emit('played', this.track)
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white mt-8">
+        <div class="px-6 py-4">
+            <img :src=" `storage/${track.image}` " alt="">
+            <div class="font-bold text-xl mb-2">
+               Track  {{ track.title }}
+            </div>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#music</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#track</span>
+        </div>
+        <div class="px-6 py-4">
+            <button  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="handleClick">
+                Lire
+            </button>
+        </div>
+    </div>
+</template>
+
--- resources/js/Pages/Track/Index.vue
+++ resources/js/Pages/Track/Index.vue
@@ -1,33 +1,55 @@
-<style>
-
-
-</style>
-
 <template>
     <MusicLayout/>
-    <div>
-        <h1> Liste de mes tracks </h1>
 
-        <ul>
-            <li v-for="track in tracks" :key="track.uuid">
-                {{ track.title }} ( {{ track.artist }} )
-            </li>
+    <div class="container mx-auto p-4">
+        <h1 class="text-2xl font-bold mb-4">Liste de mes tracks</h1>
 
-        </ul>
-
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <Tracks v-for="track in tracks" :key="track.uuid" :track="track" @played="play" />
+        </div>
     </div>
-
 </template>
 
 <script>
 import MusicLayout from "@/Layouts/MusicLayout.vue";
+import Tracks from "@/Components/Track.vue";
 
 export default {
-    name : 'Index',
-    components: {MusicLayout},
-    props: {
-        tracks : Array,
+    name: 'Index',
+    components: {
+        MusicLayout,
+        Tracks,
     },
+    data (){
+        return{
+            audio : null,
+            currentTrack : null,
+
+        }
+    },
+    props: {
+        tracks: Array,
+    },
+    methods: {
+        play(track){
+            const url = 'storage/' + track.music;
+
+
+            if (!this.currentTrack){
+                this.audio = new Audio(url)
+                this.audio.play()
+            }else if (this.currentTrack !== track.uuid){
+                this.audio.pause()
+                this.audio.src = url
+                this.audio.play()
+            }else {
+                this.audio.paused ? this.audio.play() : this.audio.pause();
+            }
+
+            this.currentTrack = track.uuid
+            this.audio.addEventListener('ended', () => this.currentTrack = null );
+        }
+    }
 }
 </script>
 
