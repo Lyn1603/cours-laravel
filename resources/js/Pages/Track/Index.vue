06bd9cc1d11db7eed38418253a83ef6e2870b0c5
Lynda <lylyben74@gmail.com>
2024-07-09 09:18:01 +0200

    init project

---
+++ resources/js/Pages/Track/Index.vue
@@ -0,0 +1,34 @@
+<style>
+
+
+</style>
+
+<template>
+    <MusicLayout/>
+    <div>
+        <h1> Liste de mes tracks </h1>
+
+        <ul>
+            <li v-for="track in tracks" :key="track.uuid">
+                {{ track.title }} ( {{ track.artist }} )
+            </li>
+
+        </ul>
+
+    </div>
+
+</template>
+
+<script>
+import MusicLayout from "@/Layouts/MusicLayout.vue";
+
+export default {
+    name : 'Index',
+    components: {MusicLayout},
+    props: {
+        tracks : Array,
+    },
+}
+</script>
+
+
