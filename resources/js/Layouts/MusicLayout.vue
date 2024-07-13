4d8566b5e2c3134788491391e130c9ca202a4b36
Lynda <lylyben74@gmail.com>
2024-07-13 23:41:46 +0200

    add btn play

--- resources/js/Layouts/MusicLayout.vue
+++ resources/js/Layouts/MusicLayout.vue
@@ -2,10 +2,16 @@
     <div>
         <nav class="bg-[#c3b377] shadow rounded-lg">
             <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                <div class="relative flex h-16 items-center justify-between">
+
+                <div class="relative flex h-16 items-center justify-center"> <!-- Change justify-between to justify-center -->
+                    <div class="flex justify-left align-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 ml-[50%]" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 19V6l12-2v13M9 10l12-2m-6 10h.01M6 17h.01M6 13h.01M6 9h.01" />
+                        </svg>
+                    </div>
                     <div>
-                        <div class=" hidden sm:ml-6 sm:block">
-                            <div class=" text-center flex space-x-4">
+                        <div class="hidden sm:ml-6 sm:block">
+                            <div class="text-center flex space-x-4">
                                 <!-- New links for music and playlists -->
                                 <Link v-if="$page.props.auth.user" :href="route('tracks.index')"
                                       class="text-[#f6ed95] font-bold rounded py-2 px-4 bg-c3b377 hover:text-gray-800 hover:bg-f6ed95"
@@ -40,7 +46,6 @@
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </nav>
